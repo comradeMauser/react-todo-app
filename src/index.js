4229eec4ade5db267fc2a2792d10735ff2eeb274
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-17 22:50:48 +0300

    first step

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.error("[eq!")
+import React from "react";
+import ReactDom from "react-dom"
+
+
+const element = (
+    <div>
+        <h1>Heavy Metal!</h1>
+        <input placeholder={"search"}/>
+        <ul>
+            <li>drink all coffee</li>
+            <li>run in circles and scream</li>
+            <li>do something</li>
+        </ul>
+    </div>
+);
+
+ReactDom.render(element, document.getElementById("root"))
