5c211ae2db4f8f39cebf160731e65e8939005372
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-22 17:04:10 +0300

    upd searchPanel, buttons added

--- src/search.js
+++ src/search.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const SearchPanel = () => {
     return (
-        <div style={{marginLeft: "20px"}}>
-            <input placeholder="search"/>
+        <div>
+            <div style={{marginLeft: "20px"}} className="form-inline">
+                <input className="form-control" placeholder="search"/>
+                <div className="btn-group-append" role="group">
+                    <button type="button" className="btn btn-light">first</button>
+                    <button type="button" className="btn btn-light">second</button>
+                    <button type="button" className="btn btn-light">third</button>
+                </div>
+            </div>
         </div>
     )
 };
-
 
 export default SearchPanel;
