a7c5afdb4e902a5bc8bf36c202ced8699f31f398
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-24 18:49:58 +0300

    upd

--- src/components/list.js
+++ src/components/list.js
@@ -3,10 +3,13 @@ import ListItem from "./list-item";
 import "./list.css";
 
 const List = () => {
+    
     const elements = [
         {id: 0, label: "drink all coffee", important: true},
         {id: 1, label: "run in circles and scream", important: false},
         {id: 2, label: "do something", important: false},
+        {id: 3, label: "sum summus mus", important: false},
+
     ]
     return (
         <div>
