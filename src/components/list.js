224ef1c13ef77dc26fa6f2377ec2524ffecec373
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-30 14:26:56 +0300

    upd

--- src/components/list.js
+++ src/components/list.js
@@ -4,29 +4,34 @@ import "./list.css";
 
 const List = () => {
 
-    const elements = [
-        {id: 0, label: "drink all coffee", important: true},
-        {id: 1, label: "run in circles and scream", important: false},
-        {id: 2, label: "do something", important: false},
-        {id: 3, label: "sum summus mus", important: false},
+        const elements = [
+            {id: 0, label: "drink all coffee", important: true},
+            {id: 1, label: "run in circles and scream", important: false},
+            {id: 2, label: "do something", important: true},
+            {id: 3, label: "sum summus mus", important: false},
 
-    ];
+        ];
 
-    const list = elements.map((element) => {
-        return (
-            <span key={element.id}>
-                <ListItem {...element}/>
+        const list = elements.map((element) => {
+
+            const star = () => {
+                console.log("star", element.id)
+            }
+            return (
+                <span key={element.id}>
+                <ListItem {...element} star={star}/>
             </span>
-        )
-    });
+            )
+        });
 
-    return (
-        <div>
-            <ul className="list-group">
-                <li style={{listStyleType: "none"}}>{list}</li>
-            </ul>
-        </div>
-    )
-};
+        return (
+            <div>
+                <ul className="list-group">
+                    <li style={{listStyleType: "none"}}>{list}</li>
+                </ul>
+            </div>
+        )
+    }
+;
 
 export default List;
