e28ed872811c7f5c3aee98352b1b349ae9f858e7
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-30 14:27:15 +0300

    upd

---
+++ src/components/search-panel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./search.css";
+
+export default class SearchPanel extends React.Component {
+
+    render() {
+        return (
+            <div className="form-inline justify-content-center">
+                <input className="form-control" placeholder="search"/>
+                <div className="btn-group-append" role="group">
+                    <button type="button" className="btn btn-dark">first</button>
+                    <button type="button" className="btn btn-dark">second</button>
+                    <button type="button" className="btn btn-dark">third</button>
+                </div>
+            </div>
+        )
+    }
+};
