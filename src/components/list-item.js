efa243bfed2c78dd5f3f2ad3cbf9460c6ad1db4d
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-07-31 15:59:12 +0300

    constructor added

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -4,8 +4,32 @@ import "./list-item.css"
 
 export default class ListItem extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            done: false
+        }
+
+        this.check = () => {
+            console.log("check", this.props.id);
+            this.setState(({done})=>{
+                return{
+                    done: !done
+                }
+            })
+        }
+    }
+
     render() {
-        const {label, important, id, star} = this.props
+        const {label, important, id} = this.props
+        // const
+
+        let colClassNames = "col"
+        if (this.state.done) {
+            colClassNames += " done"
+        }
+
         const style = {
             color: important ? "red" : "",
             fontWeight: important ? "bold" : "",
@@ -17,10 +41,14 @@ export default class ListItem extends React.Component {
         return (
             <div className="list-group-item">
                 <div className="row">
-                <span className="col" style={style} onClick={() => {star()}}>
+                <span className={colClassNames} style={style}
+                      onClick={() => {
+                          this.check()
+                      }}
+                >
                     {label}
                 </span>
-                    <span className="col"><ItemButtons check={star} id={id}/></span>
+                    <span className="col"><ItemButtons check={this.check} id={id}/></span>
                 </div>
             </div>
         );
