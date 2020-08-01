cc97f1061aafc7f0ba7a1fd4a8493a76c0c5a45c
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-08-01 21:16:53 +0300

    data simplify

--- src/components/list.js
+++ src/components/list.js
@@ -5,11 +5,10 @@ import "./list.css";
 const List = () => {
 
         const elements = [
-            {id: 0, label: "drink all coffee", important: true},
-            {id: 1, label: "run in circles and scream", important: false},
-            {id: 2, label: "do something", important: false},
-            {id: 3, label: "sum summus mus", important: false},
-
+            {id: 0, label: "drink all coffee"},
+            {id: 1, label: "run in circles and scream"},
+            {id: 2, label: "do something"},
+            {id: 3, label: "sum summus mus"},
         ];
 
         const list = elements.map((element) => {
