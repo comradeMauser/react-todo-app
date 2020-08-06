c8ea7f4730f25df5b393cb24004ba8053a6c2068
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-08-06 19:19:06 +0300

    update

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -3,10 +3,15 @@ import "./search-panel.css";
 
 export default class SearchPanel extends React.Component {
 
+    state = {
+        formSearch: ""
+    }
+
     render() {
         return (
             <div className="form-inline justify-content-center">
-                <input className="form-control" placeholder="search"/>
+                <input className="form-control" placeholder="search"
+                       onChange={this.props.formChange}/>
                 <div className="btn-group-append" role="group">
                     <button type="button" className="btn btn-dark">first</button>
                     <button type="button" className="btn btn-dark">second</button>
