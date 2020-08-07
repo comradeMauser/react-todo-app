4af313917f6c203b7688b3e464c55d9162887199
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-08-07 20:24:10 +0300

    refactor

--- src/components/list.js
+++ src/components/list.js
@@ -3,19 +3,17 @@ import ListItem from "./list-item";
 import "./list.css";
 
 const List = (props) => {
-
         const {listElements, onDeleted, star, check} = props
 
         const list = listElements.map((element) => {
-
             return (
                 <span key={element.id}>
-                <ListItem {...element}
-                          onDeleted={()=>{onDeleted(element.id)}}
-                          star={()=>{star(element.id)}}
-                          check={()=>{check(element.id)}}
-                          />
-            </span>
+                    <ListItem {...element}
+                              onDeleted={() => {onDeleted(element.id)}}
+                              star={() => {star(element.id)}}
+                              check={() => {check(element.id)}}
+                    />
+                </span>
             )
         });
 
